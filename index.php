867e873f1e28e7d810883717c615774e4ce888ff
fcjailybo123456 <fcjailybo@163.com>
2015-01-20 23:58:09 +0800

    It's just a start.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+	echo 'I am Shadown.<br>';
+	echo 'Now, I need to learn how to communicate with human and other robots.<br>';
+	echo 'My Father said:"if you want to change the world, change yourself first."';
+	
+?>
